09566cd16c55594ae45a4ec5c6222f9d55d30788
mudits02 <mudits02@gmail.com>
2024-12-10 23:14:01 +0530

    Added like operator

---
+++ New_practice/Pattern_Matching.sql
@@ -0,0 +1,25 @@
+SELECT order_id, order_date, customer_name
+FROM orders
+WHERE customer_name LIKE 'Chris%'
+
+SELECT order_id, order_date, customer_name
+FROM orders
+WHERE customer_name LIKE '%Schild';
+
+SELECT order_id, order_date, customer_name
+FROM orders
+WHERE customer_name LIKE '%hris%'
+
+SELECT order_id, order_date, customer_name
+FROM orders
+WHERE UPPER(customer_name) LIKE 'A%A';
+
+SELECT order_id, order_date, customer_name
+FROM orders
+WHERE UPPER(customer_name) LIKE '_L%A'; --Means First letter can by anytning , then it shoild have L , then anything and it should end with A
+
+SELECT order_id, order_date, customer_name
+FROM orders
+WHERE UPPER(customer_name) LIKE '__L%A'; --First 2 can be anything , then l , then Anything , then A
+
+
