3f3a9a5f5886125b034f6da14a0c38b35e3f2c59
mudits02 <mudits02@gmail.com>
2024-12-24 00:31:12 +0530

    Self Join done

---
+++ New_practice/5SelfJoinDateStringFuncCaseWhen.sql
@@ -0,0 +1,20 @@
+-- Self Join , Date and String function, Case/When statement
+
+SELECT * FROM orders;
+SELECT * FROM returns;
+SELECT * FROM employee;
+SELECT * FROM dept;
+
+-- Self Join
+
+SELECT e1.emp_id, e1.emp_name, e2.emp_name as manager_name  FROM
+employee e1
+INNER JOIN employee e2 ON e1.manager_id = e2.emp_id
+
+-- Print those employee whose salary is greater than their managers salary
+
+SELECT e1.emp_id, e1.emp_name as manager_name  FROM
+employee e1
+INNER JOIN employee e2 ON e1.manager_id = e2.emp_id
+WHERE e1.salary >= e2.salary
+
