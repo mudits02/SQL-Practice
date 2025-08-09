e0bfd476b18afa471761c01afa317a35837fd5eb
mudits02 <mudits02@gmail.com>
2025-08-09 11:04:54 +0530

    Completed Self join and CASE, WHEN

--- New_practice/6_self_join_date_string_case_when.sql
+++ New_practice/6_self_join_date_string_case_when.sql
@@ -33,3 +33,35 @@ FROM orders;
 -- In postgres , we dont have function like DATEADD or DATENAME
 -- For DATENAME , refer -> TO_CHAR , For DATEADD , directly use arthemetic operation , +, - , /
 -- For DATEDIFF -> SELECT '2025-08-01'::date - '2025-07-20'::date AS days_difference;
+
+-- CASE , WHEN, ELSE, END
+-- CASE statements are always Top to Bottom , so 300 satisfies both condition 1 and 2 , so 300 goes for Medium Profit
+
+SELECT order_id, profit,
+CASE
+	WHEN profit < 100 THEN 'Low Profit'
+	WHEN profit < 250 THEN 'Medium Profit'
+	WHEN profit < 400 THEN 'High Profit'
+ELSE
+	'Very High Profit'
+END
+FROM orders;
+
+-- Group By used in CASE, WHEN , we kind of treat the CASE WHEN as a col.
+
+SELECT 
+    CASE
+        WHEN profit < 100 THEN 'Low Profit'
+        WHEN profit < 250 THEN 'Medium Profit'
+        WHEN profit < 400 THEN 'High Profit'
+        ELSE 'Very High Profit'
+    END AS profit_category,
+    COUNT(*) AS order_count
+FROM orders
+GROUP BY 
+    CASE
+        WHEN profit < 100 THEN 'Low Profit'
+        WHEN profit < 250 THEN 'Medium Profit'
+        WHEN profit < 400 THEN 'High Profit'
+        ELSE 'Very High Profit'
+    END;
